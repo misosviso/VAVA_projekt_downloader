92fba7e71dab2cda9c49ff59f58e0d4f114e32f8
misosviso <56072239+misosviso@users.noreply.github.com>
2021-04-26 17:29:49 +0200

    RegEx

---
+++ sk/stu/fiit/models/URLValidator.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sk.stu.fiit.models;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Class which validates URL address with the usage of regex
+ * @author Admin
+ */
+public class URLValidator {
+    
+    public static boolean isURLValid(String urlSpec){
+        Pattern pattern = Pattern.compile("http://|https://www\\..*\\..*\\..+", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(urlSpec);
+        return matcher.find();
+    }
+    
+    public static void main(String[] args) {
+        String url = "https://www.cpx.sk/obrazok.cpx";
+        System.out.println("isURLValid(url) = " + isURLValid(url));
+    }
+    
+}
